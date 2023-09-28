6e91a8fbe70b61b156e482a4d59cc2b6f3cd9532
WillOli <william.ika@gmail.com>
2023-09-28 10:24:20 -0300

    first commit

---
+++ src/main/java/com/willdev/SantanderDev2023Application.java
@@ -0,0 +1,13 @@
+package com.willdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDev2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDev2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/willdev/SantanderDev2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDev2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
